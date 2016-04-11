668778e06825a0010c11e24adaab386d9de7385d
Florian Jaeger <fjaeger@student.tgm.ac.at>
2016-04-11 15:28:16 +0200

    Added Testfile

---
+++ jäger/TestStringTokenizer.java
@@ -0,0 +1,10 @@
+package jäger;
+
+import java.util.NoSuchElementException;
+import java.util.StringTokenizer;
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class TestStringTokenizer {
+	StringTokenizer st;
+}
