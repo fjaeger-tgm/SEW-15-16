3e9287d22324ea90730f26ab8bb32b96324fa9af
Florian Jaeger <fjaeger@student.tgm.ac.at>
2016-04-26 19:49:41 +0200

    Added testVerification

--- Mockito/src/showcase/MockupTest.java
+++ Mockito/src/showcase/MockupTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InOrder;
 
 public class MockupTest {
 	LinkedList mockedList;
@@ -98,5 +99,39 @@ public class MockupTest {
 
 		   //Aufgrund der Zeile 97 wird hier eine RuntimeException geworfen
 		   mockedList.clear();
+	}
+	
+	@Test
+	public void testVerication() {
+		 // A. Single mock whose methods must be invoked in a particular order
+		 List singleMock = mock(List.class);
+
+		 //using a single mock
+		 singleMock.add("was added first");
+		 singleMock.add("was added second");
+
+		 //create an inOrder verifier for a single mock
+		 InOrder inOrder = inOrder(singleMock);
+
+		 //following will make sure that add is first called with "was added first, then with "was added second"
+		 inOrder.verify(singleMock).add("was added first");
+		 inOrder.verify(singleMock).add("was added second");
+
+		 // B. Multiple mocks that must be used in a particular order
+		 List firstMock = mock(List.class);
+		 List secondMock = mock(List.class);
+
+		 //using mocks
+		 firstMock.add("was called first");
+		 secondMock.add("was called second");
+
+		 //create inOrder object passing any mocks that need to be verified in order
+		 InOrder inOrder = inOrder(firstMock, secondMock);
+
+		 //following will make sure that firstMock was called before secondMock
+		 inOrder.verify(firstMock).add("was called first");
+		 inOrder.verify(secondMock).add("was called second");
+
+		 // Oh, and A + B can be mixed together at will
 	}
 }
